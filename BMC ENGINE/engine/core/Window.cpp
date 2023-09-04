9ed75a67bcfab5b00670a55a10913618a4d7711d
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-03 22:41:37 -0400

    Updated Window

--- BMC ENGINE/engine/core/Window.cpp
+++ BMC ENGINE/engine/core/Window.cpp
@@ -1,1 +1,12 @@
 #include "Window.h"
+
+Window::Window(int width, int height, const char* title)
+{
+	loadVidMode();
+}
+
+void Window::loadVidMode()
+{
+	monitor = glfwGetPrimaryMonitor();
+	vidMode = glfwGetVideoMode(monitor);
+}
--- BMC ENGINE/engine/core/Window.h
+++ BMC ENGINE/engine/core/Window.h
@@ -1,5 +1,14 @@
 #pragma once
+#include "../BMCEngine.h"
+
 class Window
 {
+public:
+	Window(int width, int height, const char* title);
+private:
+	void loadVidMode();
+	GLFWwindow* window;
+	GLFWmonitor* monitor;
+	const GLFWvidmode* vidMode;
 };
 
