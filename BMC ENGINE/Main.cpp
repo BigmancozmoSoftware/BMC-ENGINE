d70cc22eac420cec87d134e66d25bfa04fc0ad80
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-03 23:00:34 -0400

    main class

--- BMC ENGINE/Main.cpp
+++ BMC ENGINE/Main.cpp
@@ -1,1 +1,13 @@
 #include "engine/BMCEngine.h"
+
+int main(){
+	glfwInit();
+
+	Window* window = new Window(800, 600, "BMC Engine");
+
+	while (!window->shouldClose()) {
+		window->update();
+	}
+
+	return 0;
+}
