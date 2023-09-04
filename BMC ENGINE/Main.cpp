9d81e70879d8a01308ef104c962c6f3a045ac3c1
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-04 15:48:11 -0400

    added file read test

--- BMC ENGINE/Main.cpp
+++ BMC ENGINE/Main.cpp
@@ -6,10 +6,14 @@ int main(){
 
 	Window* window = new Window(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT, GAME_WINDOW_TITLE);
 	Renderer* renderer = new Renderer();
+	
+	FileManager* files = new FileManager();
+
+	files->ReadFile("GameSettings.h");
 
 	renderer->init();
 	while (!window->shouldClose()) {
-		glClearColor(0, 0, 0, 1);
+		glClearColor(1, 0, 0, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		window->update();
