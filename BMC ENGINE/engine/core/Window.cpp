810df27aeee9410cc7157f442ce96551320b54e8
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-08 20:21:07 -0400

    made the icon work

--- BMC ENGINE/engine/core/Window.cpp
+++ BMC ENGINE/engine/core/Window.cpp
@@ -12,19 +12,25 @@ Window::Window(int width, int height, const char* title)
 
 	window = glfwCreateWindow(width, height, title, NULL, NULL);
 	
+	int img_width, img_height;
+	int channels;
+	unsigned char* pixels = stbi_load("./resources/bmcenginelogo.png", &img_width, &img_height, &channels, 4);
 
+	if (stbi_failure_reason()) {
+		std::cout << "Image failed to load: " << stbi_failure_reason() << endl;
+	}
 
 	GLFWimage images[1];
-	images[0].height = 64;
-	images[0].width = 64;
-	
-
-	glfwSetWindowIcon(window, 0, NULL);
+	images[0].height = img_height;
+	images[0].width = img_width;
+	images[0].pixels = pixels;
 
 	glfwSetWindowPos(window, (vidMode->width / 2) - (width / 2), (vidMode->height / 2) - (height / 2));
 	glfwShowWindow(window);
 
 	glfwMakeContextCurrent(window);
+
+	glfwSetWindowIcon(window, 1, images);
 }
 
 void Window::update()
