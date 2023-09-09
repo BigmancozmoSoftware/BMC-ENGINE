f1793db375a025f476450c30c89461a311e7638a
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-08 20:40:50 -0400

    Fullscreen option - doesnt work

--- BMC ENGINE/GameSettings.h
+++ BMC ENGINE/GameSettings.h
@@ -4,3 +4,4 @@
 #define GAME_WINDOW_WIDTH 1270 // Set to 0 for auto
 #define GAME_WINDOW_HEIGHT 720 // Set to 0 for auto
 #define GAME_WINDOW_TITLE "BMC Engine"
+#define GAME_WINDOW_FULLSCREEN false
