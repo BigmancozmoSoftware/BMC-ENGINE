c8c4e2287bf7a65bbda6867a10ca4c5490a8dbd9
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-08 20:42:47 -0400

    set to 0

--- BMC ENGINE/GameSettings.h
+++ BMC ENGINE/GameSettings.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Game Window //
-#define GAME_WINDOW_WIDTH 1270 // Set to 0 for auto
-#define GAME_WINDOW_HEIGHT 720 // Set to 0 for auto
+#define GAME_WINDOW_WIDTH 0 // Set to 0 for auto
+#define GAME_WINDOW_HEIGHT 0 // Set to 0 for auto
 #define GAME_WINDOW_TITLE "BMC Engine"
 #define GAME_WINDOW_FULLSCREEN false
