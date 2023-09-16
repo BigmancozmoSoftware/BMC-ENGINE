b378b16a3a798c77d47855a419b6e7cbd3faddd9
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-15 21:17:14 -0400

    fix

--- finalize.bat
+++ finalize.bat
@@ -7,7 +7,25 @@ echo:
 echo Step 1: Copy assets
 echo:
 
+echo Clearing existing resources
 
+cd x64/Debug
+rmdir resources /s /q
+mkdir resources
+cd ../Release
+rmdir resources /s /q
+mkdir resources
+cd ../..
+
+cd "BMC ENGINE"
+cd engine
+
+echo Copying to Debug
+robocopy "assets" "../../x64/Debug/resources" /s /e
+
+echo Copying to Release
+robocopy "assets" "../../x64/Release/resources" /s /e
+pause
 
 echo:
 echo Step 2: Delete unnecessary files
