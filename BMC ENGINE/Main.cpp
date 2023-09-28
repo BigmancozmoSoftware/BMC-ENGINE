22d5021d5b129d8cd095e2e240801e36bb5609a4
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-09-27 20:27:09 -0400

    changed volume

--- BMC ENGINE/Main.cpp
+++ BMC ENGINE/Main.cpp
@@ -15,7 +15,7 @@ int main(){
 	// Check GameSettings.h for game config.
 	Game* game = new Game();
 
-	game->audio->setVolume(10);
+	game->audio->setVolume(25);
 	game->audio->playSound("./resources/sound/bass.mp3");
 
 	Color* bgColor = new Color(58, 96, 158);
