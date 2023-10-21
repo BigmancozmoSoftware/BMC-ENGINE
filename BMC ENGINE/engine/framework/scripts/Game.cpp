f09f9d992151a9afad3f38958395421a1d174acc
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-10-21 09:10:12 -0400

    Cleanup function

--- BMC ENGINE/engine/framework/scripts/Game.cpp
+++ BMC ENGINE/engine/framework/scripts/Game.cpp
@@ -3,10 +3,16 @@
 Game::Game()
 {
 	audio = new Audio();
-	//renderer = new Renderer();
+	renderer = new VulkanRenderer();
 	math = new Math();
 	window = new Window(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT, GAME_WINDOW_TITLE);
 	files = new FileManager();
 	keyboard = new KeyboardInput(window->getWindow());
 	mouse = new MouseInput(window->getWindow());
+}
+
+Game::~Game()
+{
+	glfwTerminate();
+	renderer->~VulkanRenderer();
 }
