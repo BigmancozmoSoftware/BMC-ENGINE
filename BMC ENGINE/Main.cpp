ef0922d53254c4f16bd7199da39274dc35b1085c
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-10-21 09:10:30 -0400

    Removed old renderer code

--- BMC ENGINE/Main.cpp
+++ BMC ENGINE/Main.cpp
@@ -4,6 +4,7 @@
 #include "FMOD/fmod.h"
 #include <Windows.h>
 #include "PrivateData.h"
+#include <vector>
 
 int main(){
 	glfwInit();
@@ -25,68 +26,14 @@ int main(){
 	GLfloat vertices[] = {
 		-0.75f, -0.75f, 0.0f,
 		-0.25f, -0.75f, 0.0f,
-		-0.25f, 0.25f, 0.0f//,
-		//1.0f, 1.0f, 0.0f,
+		-0.25f, 0.25f, 0.0f
 	};
 
-	// RENDERER INIT START
-
-	if (glfwVulkanSupported()) {
-		cout << "Vulkan is supported" << endl;
-		cout << "" << endl;
-	}
-	
-	VkInstance instance;
-
-	VkApplicationInfo appInfo{};
-	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName = "Hello Triangle";
-	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.pEngineName = "No Engine";
-	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.apiVersion = VK_API_VERSION_1_0;
-
-	VkInstanceCreateInfo createInfo{};
-	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.pApplicationInfo = &appInfo;
-
-	uint32_t glfwExtensionCount = 0;
-	const char** glfwExtensions;
-
-	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-	createInfo.enabledExtensionCount = glfwExtensionCount;
-	createInfo.ppEnabledExtensionNames = glfwExtensions;
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::vector<VkExtensionProperties> extensions(extensionCount);
-
-	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-		throw std::runtime_error("failed to create instance!");
-	}
-
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-	cout << "Available Vulkan Extensions:" << endl;
-
-	for (const auto& extension : extensions) {
-		cout << '\t' << extension.extensionName << endl;
-	}
-
-	// RENDERER INIT END
-
 	while (!glfwWindowShouldClose(game->window->getWindow())) {
-		//game->renderer->setBackgroundColor(bgColor);
-
-		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		game->window->update();
 	}
 	
-	//game->renderer->cleanup();
-
-	glfwTerminate();
+	game->~Game();
 
 	return 0;
 }
