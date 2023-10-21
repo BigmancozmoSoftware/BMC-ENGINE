69bb06d487581e3d4d00776eaa9e8e16e85182c1
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-10-21 09:10:37 -0400

    Updated for VulkanRenderer

--- BMC ENGINE/engine/BMCEngine.h
+++ BMC ENGINE/engine/BMCEngine.h
@@ -10,7 +10,7 @@
 #include <vulkan/vulkan.h>
 #include "core/rendering/Window.h"
 #include "core/rendering/Window.h"
-#include "core/rendering/Renderer.h"
+#include "core/rendering/VulkanRenderer.h"
 #include "core/file/FileManagement.h"
 #include "framework/Math.h"
 #include "framework/input/KeyboardInput.h"
