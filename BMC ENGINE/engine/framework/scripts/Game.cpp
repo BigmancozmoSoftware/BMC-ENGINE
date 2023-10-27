874712bc83c00b522941d0bc6e31487e8fc013e4
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-10-27 07:09:09 -0400

    changed "this" to "window"

--- BMC ENGINE/engine/framework/scripts/Game.cpp
+++ BMC ENGINE/engine/framework/scripts/Game.cpp
@@ -3,12 +3,12 @@
 Game::Game()
 {
 	audio = new Audio();
-	renderer = new VulkanRenderer(this);
 	math = new Math();
 	window = new Window(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT, GAME_WINDOW_TITLE);
 	files = new FileManager();
 	keyboard = new KeyboardInput(window->getWindow());
 	mouse = new MouseInput(window->getWindow());
+	renderer = new VulkanRenderer(window);
 }
 
 Game::~Game()
