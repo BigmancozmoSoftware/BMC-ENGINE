096956230aace2db180e78845759253b6f78427c
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-11-13 20:03:39 -0500

    yay triangle

--- BMC ENGINE/Main.cpp
+++ BMC ENGINE/Main.cpp
@@ -30,22 +30,71 @@ int main(){
 	}
 	glViewport(0, 0, game->window->width, game->window->height);
 
+	
+
+	// DEFINITIONS
 	string vertexShaderStr = game->files->ReadFile("./resources/shaders/default/vertexShader.vert");
 	string fragmentShaderStr = game->files->ReadFile("./resources/shaders/default/fragmentShader.frag");
 	const char* vertexShaderSource = vertexShaderStr.c_str();
 	const char* fragmentShaderSource = fragmentShaderStr.c_str();
-
 	unsigned int vertexShader;
 	unsigned int fragmentShader;
+	unsigned int shaderProgram;
+	unsigned int VBO, VAO;
+	int success;
+	char infoLog[512];
+
+	// VS/FS
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Shader program
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// Delete shaders, they're in the program
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
+		// Triangle 1
 		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f,
 	};
 
+	// VBO/VAO
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(game->window->getWindow())) {
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(0);
+
 		game->window->update();
 	}
 	
