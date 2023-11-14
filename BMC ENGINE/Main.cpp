f2602183305b6eac801c5e9e1a757364412255c4
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-11-13 19:30:19 -0500

    opengl stuff

--- BMC ENGINE/Main.cpp
+++ BMC ENGINE/Main.cpp
@@ -23,6 +23,13 @@ int main(){
 
 	Color* bgColor = new Color(58, 96, 158);
 
+	// init glad
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, game->window->width, game->window->height);
+
 	string vertexShaderStr = game->files->ReadFile("./resources/shaders/default/vertexShader.vert");
 	string fragmentShaderStr = game->files->ReadFile("./resources/shaders/default/fragmentShader.frag");
 	const char* vertexShaderSource = vertexShaderStr.c_str();
