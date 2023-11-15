65c53f982bc7807ee3b9051974b1f40090f443ff
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2023-11-14 20:38:07 -0500

    SETTINGS_ANTIALIASING = true

--- BMC ENGINE/GameSettings.h
+++ BMC ENGINE/GameSettings.h
@@ -5,3 +5,6 @@
 #define GAME_WINDOW_HEIGHT 0 // Set to 0 for auto
 #define GAME_WINDOW_TITLE "BMC Engine"
 #define GAME_WINDOW_FULLSCREEN false
+
+// Game Default Settings //
+#define SETTINGS_ANTIALIASING true
